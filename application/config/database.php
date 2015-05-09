e2ea8675b7c179136cd03fc53ed9a7f67602ac36
chandra92kurniawan <chakz790@gmail.com>
2015-05-09 18:01:26 +0700

    db

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'lab';
-$db['default']['password'] = 'aingganteng';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
 $db['default']['database'] = 'lab';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
